a53ada2e41fb67d825977cf66956c0aeaa99235b
Bosireri <bosireri24@gmail.com>
2023-06-02 14:50:41 +0300

    manage

---
+++ src/test/java/tech/getarrays/ActuatorEmp/Manager/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.getarrays.ActuatorEmp.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
