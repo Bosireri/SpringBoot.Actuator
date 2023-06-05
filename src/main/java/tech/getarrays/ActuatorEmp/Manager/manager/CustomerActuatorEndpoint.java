0875280836636885b487979c3fa6fe3022e88030
Bosireri <bosireri24@gmail.com>
2023-06-05 15:23:39 +0300

    newEndpoint

---
+++ src/main/java/tech/getarrays/ActuatorEmp/Manager/manager/CustomerActuatorEndpoint.java
@@ -0,0 +1,20 @@
+package tech.getarrays.ActuatorEmp.Manager.manager;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Endpoint(id = "custom")
+@Component
+public class CustomerActuatorEndpoint {
+
+    @ReadOperation
+    public Map<String, String> customEndpoint() {
+        Map<String, String> map = new HashMap<>();
+        map.put("Key", "Value");
+        return map;
+    }
+}
